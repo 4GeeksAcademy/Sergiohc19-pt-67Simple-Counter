3e3c425eb8b97c82b1a6a8b19e40f64b1eba125d
Sergiohc19 <161878354+Sergiohc19@users.noreply.github.com>
2024-06-08 13:57:20 +0000

    Proyecto terminado

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,16 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+
+import SecondsCounter from "./component/home.jsx";
+
+
+
+  
+  ReactDOM.render(<SecondsCounter  />, 
+    document.querySelector("#app"));
+  
+
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
